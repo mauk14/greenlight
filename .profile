5885ef77c1fe2046457f5e9131ef080bf2a36fed
mauk14 <mnd33599@gmail.com>
2022-12-23 16:29:59 +0600

    5 6 chapter

---
+++ .profile
@@ -0,0 +1,1 @@
+export GREENLIGHT_DB_DSN='postgres://postgres:N2704Anice@localhost:5432/greenlight?sslmode=disable'
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -26,9 +34,25 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -46,7 +70,24 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
 
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.Config()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
